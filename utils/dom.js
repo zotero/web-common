d067b3389a24821d45936740bf8ca4c9b29173cc
Tom Najdek <tom@doppnet.com>
2023-07-03 16:35:06 +0200

    Add getScrollbarWidth() util

--- utils/dom.js
+++ utils/dom.js
@@ -12,3 +12,20 @@ export const scrollIntoViewIfNeeded = (element, container, opts = {}) => {
     }
     return 0;
 }
+
+export const getScrollbarWidth = () => {
+	if (process.env.NODE_ENV === 'test') {
+		return 0;
+	}
+	const scrollDiv = document.createElement('div');
+	scrollDiv.style.position = 'absolute';
+	scrollDiv.style.top = '-9999px';
+	scrollDiv.style.width = '50px';
+	scrollDiv.style.height = '50px';
+	scrollDiv.style.overflow = 'scroll';
+	document.body.appendChild(scrollDiv);
+	const scrollbarWidth = scrollDiv.getBoundingClientRect().width - scrollDiv.clientWidth;
+	document.body.removeChild(scrollDiv);
+
+	return scrollbarWidth;
+}
