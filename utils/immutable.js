5f7e0def8497632e4bbbb7115dfd63e9656cdd55
Tom Najdek <tom@doppnet.com>
2023-08-23 16:52:24 +0200

    Enhance `omit` util to accept filter function

--- utils/immutable.js
+++ utils/immutable.js
@@ -1,13 +1,18 @@
 const omit = (object, deleteKeys) => {
-	if(!Array.isArray(deleteKeys)) {
-		deleteKeys = [deleteKeys];
+	if(typeof(deleteKeys) !== 'function') {
+		if(!Array.isArray(deleteKeys)) {
+			deleteKeys = [deleteKeys];
+		}
+		deleteKeys = deleteKeys.map(dk => typeof(dk) !== 'string' ? dk.toString() : dk);
 	}
-
-	deleteKeys = deleteKeys.map(dk => typeof(dk) !== 'string' ? dk.toString() : dk);
 
 	return Object.entries(object)
 		.reduce((aggr, [key, value]) => {
-			if(!deleteKeys.includes(key)) { aggr[key] = value; }
+			if(typeof (deleteKeys) === 'function') {
+				if(!deleteKeys(key, value)) { aggr[key] = value; }
+			} else if(!deleteKeys.includes(key)) {
+				aggr[key] = value;
+			}
 			return aggr;
 	}, {});
 }
