64e9754400aeb7651303481c6089da66e6d6896a
Tom Najdek <tom@doppnet.com>
2024-05-06 15:20:02 +0200

    Remove use of `defaultProps`

--- components/button.jsx
+++ components/button.jsx
@@ -2,8 +2,6 @@ import cx from 'classnames';
 import PropTypes from 'prop-types';
 import { forwardRef, memo } from 'react';
 
-import { noop } from '../utils/common';
-
 const Button = memo(forwardRef((props, ref) => {
 	const { children, className, icon, ...rest } = props;
 		const classNames = cx('btn', className, {
@@ -22,19 +20,10 @@ const Button = memo(forwardRef((props, ref) => {
 
 Button.displayName = "Button";
 
-Button.defaultProps = {
-	onBlur: noop,
-	onClick: noop,
-	onFocus: noop,
-};
-
 Button.propTypes = {
 	children: PropTypes.node,
 	className: PropTypes.string,
-	icon: PropTypes.bool,
-	onBlur: PropTypes.func,
-	onClick: PropTypes.func,
-	onFocus: PropTypes.func,
+	icon: PropTypes.bool
 };
 
 export { Button };
